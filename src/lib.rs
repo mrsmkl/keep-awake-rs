2f03ef56f6a2edcbdefdb7595bbf848f76ac2cec
Sami Mäkelä <mrsmkl@gmail.com>
2020-11-03 17:45:21 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,6 @@ pub fn inhibit(name: &str, reason: &str) -> Result<Holder, Box<dyn std::error::E
 
     let m = Message::new_method_call("org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver", "org.freedesktop.ScreenSaver", "Inhibit")?
       .append2(name, reason);
-    let r = conn.send_with_reply_and_block(m, Duration::from_millis(2000))?;
-    println!("{:?}", r);
-    // std::thread::sleep(Duration::from_millis(2000*1000));
+    conn.send_with_reply_and_block(m, Duration::from_millis(2000))?;
     Ok(Holder{conn: conn})
 }
