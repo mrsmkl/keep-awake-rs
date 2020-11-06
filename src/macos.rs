9a6959464ee4542a7b39ef0bd714ef930798b13d
Sami Mäkelä <mrsmkl@gmail.com>
2020-11-06 17:07:08 +0200

    cleanup

--- src/macos.rs
+++ src/macos.rs
@@ -37,8 +37,7 @@ pub fn inhibit(_name: &str, reason: &str) -> Result<Holder, Box<dyn std::error::
     let mut id : IOPMAssertionId = 0;
     let reason_cf = CFString::new(reason);
     unsafe {
-        let ret = IOPMAssertionCreateWithName(k_iopmassertion_type_prevent_system_sleep.as_concrete_TypeRef(), K_IOPMASSERTION_LEVEL_ON, reason_cf.as_concrete_TypeRef(), &mut id);
-        println!("returned {:?}, id is now {:?}", ret, id);
+        let _ret = IOPMAssertionCreateWithName(k_iopmassertion_type_prevent_system_sleep.as_concrete_TypeRef(), K_IOPMASSERTION_LEVEL_ON, reason_cf.as_concrete_TypeRef(), &mut id);
     }
     Ok(Holder { id: id })
 }
