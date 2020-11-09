63da170407e4ced0e9bbaf42b380fceea6436ea6
Sami Mäkelä <mrsmkl@gmail.com>
2020-11-09 14:28:29 +0200

    handle headless case

--- src/linux.rs
+++ src/linux.rs
@@ -6,15 +6,15 @@ pub fn inhibit(name: &str, reason: &str) -> Result<Holder, Box<dyn std::error::E
     match zbus::Connection::new_session() {
         Ok(conn) => {
             let _reply = conn
-            .call_method(
-            Some("org.freedesktop.ScreenSaver"),
-            "/org/freedesktop/ScreenSaver",
-            Some("org.freedesktop.ScreenSaver"),
-            "Inhibit",
-            &(name, reason),
-        )
-        .unwrap();
-        Ok(Holder { _conn: Some(conn) })
+                .call_method(
+                    Some("org.freedesktop.ScreenSaver"),
+                    "/org/freedesktop/ScreenSaver",
+                    Some("org.freedesktop.ScreenSaver"),
+                    "Inhibit",
+                    &(name, reason),
+                )
+                .unwrap();
+            Ok(Holder { _conn: Some(conn) })
         }
         _ => Ok(Holder { _conn: None }),
     }
